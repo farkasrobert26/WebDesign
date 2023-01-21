4224235d66ff2d3180aa527dc4d087b24af452df
RobertF <farkas_robert@yahoo.com>
2023-01-21 23:56:06 +0200

    Updated CheckIn

--- check-in.php
+++ check-in.php
@@ -1,11 +1,34 @@
 <?php
 
+// Connect to the database
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "test";
+$conn = mysqli_connect($host, $username, $password, $dbname);
 
-$name = $_POST['nameId'];
-$mysqli = new mysqli("localhost", "root", "", "test");
-$query = "INSERT INTO reservations (name, email, phone, checkin, checkout, rooms) VALUES ('$name', '$email', '$phone', '$checkin', '$checkout', '$rooms')";
-$mysqli->query($query);
-echo "Registration Successfully";
-$mysqli->close();
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
+// Get the form data
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$checkin = $_POST['checkin'];
+$checkout = $_POST['checkout'];
+$rooms = $_POST['rooms'];
+
+// Insert the data into the database
+$sql = "INSERT INTO reservations (name, email, phone, checkin, checkout, rooms)
+VALUES ('$name', '$email', '$phone', '$checkin', '$checkout', '$rooms')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
 ?>
